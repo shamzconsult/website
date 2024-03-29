dd8113963e436567f4fb237f07869681281dc9c5
eedrisofficial <eedrisofficial.gmail.com>
2024-03-29 07:00:38 -0400

    content added

---
+++ app/(default)/page.tsx
@@ -0,0 +1,22 @@
+export const metadata = {
+  title: "Shamz - Shamz consult",
+  description: "your trusted ally for your personal and organizational growth",
+};
+
+import Hero from "@/components/hero";
+import Features from "@/components/features";
+import FeaturesBlocks from "@/components/features-blocks";
+import Testimonials from "@/components/testimonials";
+import Newsletter from "@/components/newsletter";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <FeaturesBlocks />
+      <Testimonials />
+      <Newsletter />
+    </>
+  );
+}
