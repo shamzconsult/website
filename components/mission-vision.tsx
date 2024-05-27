0a33ee007e5ae10f811c0aff91e37283496b3614
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-05-27 11:11:26 +0100

    Design fixes

--- components/mission-vision.tsx
+++ components/mission-vision.tsx
@@ -2,13 +2,7 @@
 
 export default function Mission() {
   return (
-    <section className='relative'>
-      <div
-        className='absolute inset-0 bg-gray-100 pointer-events-none mb-16'
-        aria-hidden='true'
-      ></div>
-      <div className='absolute left-0 right-0 m-auto w-px p-px h-20 bg-gray-200 transform -translate-y-1/2'></div>
-
+    <section className='relative bg-slate-50 mt-5 py-7'>
       <div className='relative max-w-6xl mx-auto px-4 sm:px-6'>
         <div className='pt-12 md:pt-20'>
           {/* Section header */}
