c1499e58edeccfa29c2230932636b3fb9488a93b
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-05-27 12:53:31 +0100

    Misc fix

--- components/upcoming-event.tsx
+++ components/upcoming-event.tsx
@@ -10,7 +10,7 @@ interface EventType {
 
 export const getCurrentEvent = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/event", {
+    const res = await fetch("/api/event", {
       cache: "no-store",
     });
     if (!res.ok) {
