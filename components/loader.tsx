86a1e1d7476d32b933c3e7a6cc93dc79ac33ca50
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-05-29 07:20:44 +0100

    Misc fix

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,8 +1,6 @@
 const Loading = () => {
   return (
-    <div className="h-screen text-center">
-      <div className="mx-auto w-12 h-12 border-4 border-orange-600 border-b-transparent rounded-full inline-block box-border animate-spin"></div>
-    </div>
+    <div className='mx-auto w-12 h-12 border-4 border-orange-600 border-b-transparent rounded-full inline-block box-border animate-spin'></div>
   );
 };
 
