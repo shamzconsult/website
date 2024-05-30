8a1c7e841ce234f7546296dd1d0cb01c62fcd8be
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-05-30 14:59:51 +0100

    hero fix

--- components/events.tsx
+++ components/events.tsx
@@ -72,7 +72,7 @@ const Events = () => {
   const filteredUpcomingEvents = checkForUpcomingEvents(event);
 
   const EventPageLoader = () => (
-    <div className="min-h-[70vh] flex justify-center items-center">
+    <div className='min-h-[70vh] flex justify-center items-center'>
       <Loading />
     </div>
   );
@@ -82,10 +82,10 @@ const Events = () => {
       {loading ? (
         <EventPageLoader />
       ) : (
-        <div className="flex flex-col justify-center items-center gap-20 text-gray-600 p-8">
-          <section className="flex flex-col justify-center items-center gap-6">
-            <h1 className="font-bold text-lg">Upcoming Events</h1>
-            <div className="flex flex-wrap justify-center items-start gap-8">
+        <div className='flex flex-col justify-center items-center gap-20 text-gray-600 p-8'>
+          <section className='flex flex-col justify-center items-center gap-6'>
+            <h1 className='font-bold text-lg'>Upcoming Events</h1>
+            <div className='flex flex-wrap justify-center items-start gap-8'>
               {filteredUpcomingEvents.length > 0 ? (
                 filteredUpcomingEvents.map(
                   ({ image, startDate, endDate, title, _id, isActive }) =>
@@ -93,18 +93,18 @@ const Events = () => {
                       <Link
                         href={`/events/${_id}`}
                         key={_id}
-                        className="flex flex-col gap-2 lg:w-[25%] border border-slate-100 rounded-md overflow-hidden hover:border-orange-300"
+                        className='flex flex-col gap-2 lg:w-[25%] border border-slate-100 rounded-md overflow-hidden hover:border-orange-300'
                       >
                         <img
-                          className="h-full w-full object-contain lg:h-[50vh] object-top"
+                          className='h-full w-full object-contain lg:h-[50vh] object-top'
                           src={image}
-                          alt="event"
+                          alt='event'
                         />
-                        <div className="flex flex-col gap-2 p-2">
-                          <h2 className="opacity-70 font-bold">{title}</h2>
-                          <div className="flex gap-4">
+                        <div className='flex flex-col gap-2 p-2'>
+                          <h2 className='opacity-70 font-bold'>{title}</h2>
+                          <div className='flex gap-4'>
                             <p>{formatDate(startDate)}</p>
-                            <h1 className="bold text-lg">-</h1>
+                            <h1 className='bold text-lg'>-</h1>
                             <p>{formatDate(endDate)}</p>
                           </div>
                         </div>
@@ -116,9 +116,9 @@ const Events = () => {
               )}
             </div>
           </section>
-          <section className="flex flex-col justify-center items-center gap-6">
-            <h1 className="font-bold text-lg">Past Events</h1>
-            <div className="flex flex-wrap justify-center items-start gap-8">
+          <section className='flex flex-col justify-center items-center gap-6'>
+            <h1 className='font-bold text-lg'>Past Events</h1>
+            <div className='flex flex-wrap justify-center items-start gap-8'>
               {filteredPastEvents.length > 0 ? (
                 filteredPastEvents.map(
                   ({ image, startDate, endDate, title, isActive, _id }) =>
@@ -126,18 +126,18 @@ const Events = () => {
                       <Link
                         href={`/events/${_id}`}
                         key={_id}
-                        className="flex flex-col gap-2 lg:w-[25%]  border border-slate-100 rounded-md overflow-hidden p-2 hover:border-orange-300"
+                        className='flex flex-col gap-2 lg:w-[25%] border border-slate-100 rounded-md overflow-hidden p-2 hover:border-orange-300'
                       >
                         <img
-                          className="h-full w-full object-contain object-top lg:h-[50vh]"
+                          className='h-full w-full object-contain object-top lg:h-[50vh]'
                           src={image}
-                          alt="event"
+                          alt='event'
                         />
-                        <div className="flex flex-col gap-2 p-2">
-                          <h2 className="opacity-70 font-bold">{title}</h2>
-                          <div className="flex gap-4">
+                        <div className='flex flex-col gap-2 p-2'>
+                          <h2 className='opacity-70 font-bold'>{title}</h2>
+                          <div className='flex gap-4'>
                             <p>{formatDate(startDate)}</p>
-                            <h1 className="bold text-lg">-</h1>
+                            <h1 className='bold text-lg'>-</h1>
                             <p>{formatDate(endDate)}</p>
                           </div>
                         </div>
