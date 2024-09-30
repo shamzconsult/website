b4e1de98206d2debf286164d5b6f34c6a05d2f0f
Seun Salau <156671873+Oseun12@users.noreply.github.com>
2024-09-30 11:08:27 +0100

    Chore: updating the site

--- app/services/careerService.ts
+++ app/services/careerService.ts
@@ -1,10 +1,9 @@
 import { getBaseUrl } from "../utils/getBaseUrl";
 
+
 export const getAllJob = async () => {
-<<<<<<< HEAD
     try {
-      const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-      // const baseUrl = getBaseUrl();
+      const baseUrl = getBaseUrl();
       const res = await fetch(`${baseUrl}/api/career`, {
         cache: "no-store",
       });
@@ -14,20 +13,6 @@ export const getAllJob = async () => {
       return res.json();
     } catch (error) {
       console.log("Error loading data", error);
-      throw error;
-=======
-  try {
-    const baseUrl = getBaseUrl();
-    const res = await fetch(`${baseUrl}/api/career`, {
-      cache: "no-store",
-    });
-    if (!res.ok) {
-      throw new Error("Error found while fetching");
->>>>>>> e8ab913ebaf5fddc6c7c66b53b0ac9d9988fb831
-    }
-    return res.json();
-  } catch (error) {
-    console.log("Error loading data", error);
-    throw error;
+      throw error; 
   }
 };
