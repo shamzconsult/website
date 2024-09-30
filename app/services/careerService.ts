e8ab913ebaf5fddc6c7c66b53b0ac9d9988fb831
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-09-30 10:54:39 +0100

    Chore: refactor getBaseUrl to run on server (window not present on server)

--- app/services/careerService.ts
+++ app/services/careerService.ts
@@ -1,19 +1,17 @@
 import { getBaseUrl } from "../utils/getBaseUrl";
 
-
 export const getAllJob = async () => {
-    try {
-      const baseUrl = getBaseUrl();
-      const res = await fetch(`${baseUrl}/api/career`, {
-        cache: "no-store",
-      });
-      if (!res.ok) {
-        throw new Error("Error found while fetching");
-      }
-      return res.json();
-    } catch (error) {
-      console.log("Error loading data", error);
-      throw error;
+  try {
+    const baseUrl = getBaseUrl();
+    const res = await fetch(`${baseUrl}/api/career`, {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Error found while fetching");
     }
-  };
-  
+    return res.json();
+  } catch (error) {
+    console.log("Error loading data", error);
+    throw error;
+  }
+};
