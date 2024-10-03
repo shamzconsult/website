9bdb751c028b18dba7a789963ca5a1f6e91312e8
Seun Salau <156671873+Oseun12@users.noreply.github.com>
2024-10-03 12:11:32 +0100

    Chore: design updates

--- components/our-service.tsx
+++ components/our-service.tsx
@@ -39,8 +39,8 @@ export default function Services() {
             </div>
 
             {/* 2nd item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl hover:scale-105 hover:-translate-y-2 hover:shadow-xl duration-500">
-            <div className="bg-slate-200 rounded-full">
+            <div className="relative flex flex-col items-center p-8 bg-white rounded shadow-xl hover:scale-105 hover:-translate-y-2 hover:shadow-xl duration-500">
+            <div className="bg-slate-200 rounded-full mb-2">
               <FontAwesomeIcon icon={faArrowUpRightDots} className="w-16 h-16 -mt-1 text-orange-500  p-4 " />
             </div>
 
@@ -54,8 +54,8 @@ export default function Services() {
             </div>
 
             {/* 3rd item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl hover:scale-105 hover:-translate-y-2 hover:shadow-xl duration-500">
-              <div className="bg-slate-200 rounded-full">
+            <div className="relative flex flex-col items-center p-8 bg-white rounded shadow-xl hover:scale-105 hover:-translate-y-2 hover:shadow-xl duration-500">
+              <div className="bg-slate-200 rounded-full mb-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-16 h-16 -mt-1 text-orange-500  p-4 " viewBox="0 0 16 16">
                   <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m.256 7a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1zm3.63-4.54c.18-.613 1.048-.613 1.229 0l.043.148a.64.64 0 0 0 .921.382l.136-.074c.561-.306 1.175.308.87.869l-.075.136a.64.64 0 0 0 .382.92l.149.045c.612.18.612 1.048 0 1.229l-.15.043a.64.64 0 0 0-.38.921l.074.136c.305.561-.309 1.175-.87.87l-.136-.075a.64.64 0 0 0-.92.382l-.045.149c-.18.612-1.048.612-1.229 0l-.043-.15a.64.64 0 0 0-.921-.38l-.136.074c-.561.305-1.175-.309-.87-.87l.075-.136a.64.64 0 0 0-.382-.92l-.148-.045c-.613-.18-.613-1.048 0-1.229l.148-.043a.64.64 0 0 0 .382-.921l-.074-.136c-.306-.561.308-1.175.869-.87l.136.075a.64.64 0 0 0 .92-.382zM14 12.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0"/>
                 </svg>
@@ -70,10 +70,10 @@ export default function Services() {
             </div>
 
             {/* 4th item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl hover:scale-105 hover:-translate-y-2 hover:shadow-xl duration-500">
-              <div className="bg-slate-200 rounded-full">
+            <div className="relative flex flex-col items-center p-8 bg-white rounded shadow-xl hover:scale-105 hover:-translate-y-2 hover:shadow-xl duration-500">
+              <div className="bg-slate-200 rounded-full mb-2">
               
-              <FontAwesomeIcon icon={faHelmetSafety} className="w-16 h-16 -mt-1 text-orange-500  p-4 " />              
+              <FontAwesomeIcon icon={faHelmetSafety} className="w-16 h-16 -mt-1 text-orange-500 p-4 " />              
               </div>
               <h4 className="text-xl font-bold leading-snug tracking-tight mb-1 text-orange-600">
                 Event Host/Management
@@ -84,8 +84,8 @@ export default function Services() {
             </div>
 
             {/* 5th item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl hover:scale-105 hover:-translate-y-2 hover:shadow-xl duration-500">
-              <div className="bg-slate-200 rounded-full">
+            <div className="relative flex flex-col items-center p-8 bg-white rounded shadow-xl hover:scale-105 hover:-translate-y-2 hover:shadow-xl duration-500">
+              <div className="bg-slate-200 rounded-full mb-2">
               
               <FontAwesomeIcon icon={faHandshake} className="w-16 h-16 -mt-1 text-orange-500  p-4 " />
               </div>
@@ -98,9 +98,9 @@ export default function Services() {
             </div>
 
             {/* 6th item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl hover:scale-105 hover:-translate-y-2 hover:shadow-xl duration-500">
+            <div className="relative flex flex-col items-center p-8 bg-white rounded shadow-xl hover:scale-105 hover:-translate-y-2 hover:shadow-xl duration-500">
               
-              <div className="bg-slate-200 rounded-full">
+              <div className="bg-slate-200 rounded-full mb-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-16 h-16 -mt-1 text-orange-500  p-4 " viewBox="0 0 16 16">
                   <path d="M6 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0M1 3a2 2 0 1 0 4 0 2 2 0 0 0-4 0"/>
                   <path d="M9 6h.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 7.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 16H2a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2zm6 8.73V7.27l-3.5 1.555v4.35zM1 8v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1"/>
