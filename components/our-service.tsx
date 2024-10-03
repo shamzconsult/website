50a5f4e4df6e3e243cdfe7591b438da4cde821ad
Seun Salau <156671873+Oseun12@users.noreply.github.com>
2024-10-03 11:59:09 +0100

    Chore: design update

--- components/our-service.tsx
+++ components/our-service.tsx
@@ -1,6 +1,8 @@
 // import ServiceNumber from "./service-numbers";
 
 import { ServiceNumber } from "./service-numbers";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpRightDots, faHandshake, faHelmetSafety } from '@fortawesome/free-solid-svg-icons';
 
 export default function Services() {
   return (
@@ -20,40 +22,13 @@ export default function Services() {
           <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 md:max-w-2xl items-center justify-center  lg:max-w-none">
             {/* 1st item */}
             <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl  hover:scale-105 hover:-translate-y-2 hover:shadow-xl duration-500">
-              <svg
-                className="w-16 h-16 p-1 -mt-1 mb-2"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g fill="none" fillRule="evenodd">
-                  <rect
-                    className="fill-current text-orange-600"
-                    width="64"
-                    height="64"
-                    rx="32"
-                  />
-                  <g strokeWidth="2">
-                    <path
-                      className="stroke-current text-orange-300"
-                      d="M34.514 35.429l2.057 2.285h8M20.571 26.286h5.715l2.057 2.285"
-                    />
-                    <path
-                      className="stroke-current text-white"
-                      d="M20.571 37.714h5.715L36.57 26.286h8"
-                    />
-                    <path
-                      className="stroke-current text-orange-300"
-                      strokeLinecap="square"
-                      d="M41.143 34.286l3.428 3.428-3.428 3.429"
-                    />
-                    <path
-                      className="stroke-current text-white"
-                      strokeLinecap="square"
-                      d="M41.143 29.714l3.428-3.428-3.428-3.429"
-                    />
-                  </g>
-                </g>
-              </svg>
+              <div className="bg-slate-200 rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-16 h-16 -mt-1 text-orange-500  p-4 " viewBox="0 0 16 16">
+                  <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514"/>
+                  <path d="M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v6.5a.5.5 0 0 1-1 0V1H3v14h3v-2.5a.5.5 0 0 1 .5-.5H8v4H3a1 1 0 0 1-1-1z"/>
+                  <path d="M4.5 2a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm3 0a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm3 0a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm-6 3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm3 0a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm3 0a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm-6 3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm3 0a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5z"/>
+                </svg>
+              </div>
 
               <h4 className="text-xl font-bold text-center leading-snug tracking-tight mb-1 text-orange-600">
                 Programs/Projects Management
@@ -65,38 +40,12 @@ export default function Services() {
 
             {/* 2nd item */}
             <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl hover:scale-105 hover:-translate-y-2 hover:shadow-xl duration-500">
-              <svg
-                className="w-16 h-16 p-1 -mt-1 mb-2"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g fill="none" fillRule="evenodd">
-                  <rect
-                    className="fill-current text-orange-600"
-                    width="64"
-                    height="64"
-                    rx="32"
-                  />
-                  <g strokeWidth="2" transform="translate(19.429 20.571)">
-                    <circle
-                      className="stroke-current text-white"
-                      strokeLinecap="square"
-                      cx="12.571"
-                      cy="12.571"
-                      r="1.143"
-                    />
-                    <path
-                      className="stroke-current text-white"
-                      d="M19.153 23.267c3.59-2.213 5.99-6.169 5.99-10.696C25.143 5.63 19.514 0 12.57 0 5.63 0 0 5.629 0 12.571c0 4.527 2.4 8.483 5.99 10.696"
-                    />
-                    <path
-                      className="stroke-current text-orange-300"
-                      d="M16.161 18.406a6.848 6.848 0 003.268-5.835 6.857 6.857 0 00-6.858-6.857 6.857 6.857 0 00-6.857 6.857 6.848 6.848 0 003.268 5.835"
-                    />
-                  </g>
-                </g>
-              </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1 text-orange-600">
+            <div className="bg-slate-200 rounded-full">
+              <FontAwesomeIcon icon={faArrowUpRightDots} className="w-16 h-16 -mt-1 text-orange-500  p-4 " />
+            </div>
+
+              
+              <h4 className="text-xl font-bold leading-snug mt-2 tracking-tight mb-1 text-orange-600">
                 Capacity Building
               </h4>
               <p className="text-gray-600 text-center">
@@ -106,40 +55,12 @@ export default function Services() {
 
             {/* 3rd item */}
             <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl hover:scale-105 hover:-translate-y-2 hover:shadow-xl duration-500">
-              <svg
-                className="w-16 h-16 p-1 -mt-1 mb-2"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g fill="none" fillRule="evenodd">
-                  <rect
-                    className="fill-current text-orange-600"
-                    width="64"
-                    height="64"
-                    rx="32"
-                  />
-                  <g strokeWidth="2">
-                    <path
-                      className="stroke-current text-orange-300"
-                      d="M34.743 29.714L36.57 32 27.43 43.429H24M24 20.571h3.429l1.828 2.286"
-                    />
-                    <path
-                      className="stroke-current text-white"
-                      strokeLinecap="square"
-                      d="M34.743 41.143l1.828 2.286H40M40 20.571h-3.429L27.43 32l1.828 2.286"
-                    />
-                    <path
-                      className="stroke-current text-orange-300"
-                      d="M36.571 32H40"
-                    />
-                    <path
-                      className="stroke-current text-white"
-                      d="M24 32h3.429"
-                      strokeLinecap="square"
-                    />
-                  </g>
-                </g>
-              </svg>
+              <div className="bg-slate-200 rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-16 h-16 -mt-1 text-orange-500  p-4 " viewBox="0 0 16 16">
+                  <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m.256 7a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1zm3.63-4.54c.18-.613 1.048-.613 1.229 0l.043.148a.64.64 0 0 0 .921.382l.136-.074c.561-.306 1.175.308.87.869l-.075.136a.64.64 0 0 0 .382.92l.149.045c.612.18.612 1.048 0 1.229l-.15.043a.64.64 0 0 0-.38.921l.074.136c.305.561-.309 1.175-.87.87l-.136-.075a.64.64 0 0 0-.92.382l-.045.149c-.18.612-1.048.612-1.229 0l-.043-.15a.64.64 0 0 0-.921-.38l-.136.074c-.561.305-1.175-.309-.87-.87l.075-.136a.64.64 0 0 0-.382-.92l-.148-.045c-.613-.18-.613-1.048 0-1.229l.148-.043a.64.64 0 0 0 .382-.921l-.074-.136c-.306-.561.308-1.175.869-.87l.136.075a.64.64 0 0 0 .92-.382zM14 12.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0"/>
+                </svg>
+              </div>
+
               <h4 className="text-xl font-bold leading-snug tracking-tight mb-1 text-orange-600">
                 Consultancy Services
               </h4>
@@ -150,39 +71,10 @@ export default function Services() {
 
             {/* 4th item */}
             <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl hover:scale-105 hover:-translate-y-2 hover:shadow-xl duration-500">
-              <svg
-                className="w-16 h-16 p-1 -mt-1 mb-2"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g fill="none" fillRule="evenodd">
-                  <rect
-                    className="fill-current text-orange-600"
-                    width="64"
-                    height="64"
-                    rx="32"
-                  />
-                  <g strokeWidth="2">
-                    <path
-                      className="stroke-current text-white"
-                      d="M32 37.714A5.714 5.714 0 0037.714 32a5.714 5.714 0 005.715 5.714"
-                    />
-                    <path
-                      className="stroke-current text-white"
-                      d="M32 37.714a5.714 5.714 0 015.714 5.715 5.714 5.714 0 015.715-5.715M20.571 26.286a5.714 5.714 0 005.715-5.715A5.714 5.714 0 0032 26.286"
-                    />
-                    <path
-                      className="stroke-current text-white"
-                      d="M20.571 26.286A5.714 5.714 0 0126.286 32 5.714 5.714 0 0132 26.286"
-                    />
-                    <path
-                      className="stroke-current text-orange-300"
-                      d="M21.714 40h4.572M24 37.714v4.572M37.714 24h4.572M40 21.714v4.572"
-                      strokeLinecap="square"
-                    />
-                  </g>
-                </g>
-              </svg>
+              <div className="bg-slate-200 rounded-full">
+              
+              <FontAwesomeIcon icon={faHelmetSafety} className="w-16 h-16 -mt-1 text-orange-500  p-4 " />              
+              </div>
               <h4 className="text-xl font-bold leading-snug tracking-tight mb-1 text-orange-600">
                 Event Host/Management
               </h4>
@@ -193,34 +85,10 @@ export default function Services() {
 
             {/* 5th item */}
             <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl hover:scale-105 hover:-translate-y-2 hover:shadow-xl duration-500">
-              <svg
-                className="w-16 h-16 p-1 -mt-1 mb-2"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g fill="none" fillRule="evenodd">
-                  <rect
-                    className="fill-current text-orange-600"
-                    width="64"
-                    height="64"
-                    rx="32"
-                  />
-                  <g strokeWidth="2">
-                    <path
-                      className="stroke-current text-white"
-                      d="M19.429 32a12.571 12.571 0 0021.46 8.89L23.111 23.11A12.528 12.528 0 0019.429 32z"
-                    />
-                    <path
-                      className="stroke-current text-orange-300"
-                      d="M32 19.429c6.943 0 12.571 5.628 12.571 12.571M32 24a8 8 0 018 8"
-                    />
-                    <path
-                      className="stroke-current text-white"
-                      d="M34.286 29.714L32 32"
-                    />
-                  </g>
-                </g>
-              </svg>
+              <div className="bg-slate-200 rounded-full">
+              
+              <FontAwesomeIcon icon={faHandshake} className="w-16 h-16 -mt-1 text-orange-500  p-4 " />
+              </div>
               <h4 className="text-xl font-bold leading-snug tracking-tight mb-1 text-orange-600">
                 Community Development
               </h4>
@@ -231,30 +99,15 @@ export default function Services() {
 
             {/* 6th item */}
             <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl hover:scale-105 hover:-translate-y-2 hover:shadow-xl duration-500">
-              <svg
-                className="w-16 h-16 p-1 -mt-1 mb-2"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g fill="none" fillRule="evenodd">
-                  <rect
-                    className="fill-current text-orange-600"
-                    width="64"
-                    height="64"
-                    rx="32"
-                  />
-                  <g strokeWidth="2" strokeLinecap="square">
-                    <path
-                      className="stroke-current text-white"
-                      d="M29.714 40.358l-4.777 2.51 1.349-7.865-5.715-5.57 7.898-1.147L32 21.13l3.531 7.155 7.898 1.147L40 32.775"
-                    />
-                    <path
-                      className="stroke-current text-orange-300"
-                      d="M44.571 43.429H34.286M44.571 37.714H34.286"
-                    />
-                  </g>
-                </g>
-              </svg>
+              
+              <div className="bg-slate-200 rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-16 h-16 -mt-1 text-orange-500  p-4 " viewBox="0 0 16 16">
+                  <path d="M6 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0M1 3a2 2 0 1 0 4 0 2 2 0 0 0-4 0"/>
+                  <path d="M9 6h.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 7.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 16H2a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2zm6 8.73V7.27l-3.5 1.555v4.35zM1 8v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1"/>
+                  <path d="M9 6a3 3 0 1 0 0-6 3 3 0 0 0 0 6M7 3a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/>
+                </svg>
+              </div>
+
               <h4 className="text-xl font-bold leading-snug tracking-tight mb-1 text-orange-600">
                 Content Development
               </h4>
