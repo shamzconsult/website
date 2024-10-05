b1039385d04c7a715a35fb09ea50f109217dfc6e
Seun Salau <156671873+Oseun12@users.noreply.github.com>
2024-10-05 11:17:06 +0100

    Chore: updating the UX

--- app/models/hiring.ts
+++ app/models/hiring.ts
@@ -9,6 +9,7 @@ const hiringSchema = new Schema(
     type: { type: String, enum: ALLOWED_TYPE, default: null },
     mode: { type: String, enum: ALLOWED_MODE, default: null },
     location: { type: String },
+    closing: {type: String, required: true},
     isActive: { type: Boolean, default: true },
     formId: { type: String, required: true },
     isDeleted:{type: Boolean, default: false},
